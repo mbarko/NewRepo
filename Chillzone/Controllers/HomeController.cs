4d70c4a0a745d36125731376bdb878f90adcd686
mbarko <moataz.elbarkouky@mail.utoronto.ca>
2015-10-14 06:08:48 -0700

    First comment

--- Chillzone/Controllers/HomeController.cs
+++ Chillzone/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Chillzone.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index() //first comment
         {
             return View();
         }
