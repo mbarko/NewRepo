42120207709db4ea4981413ddc8156023f48b59a
Banhawy <the.benhawy@gmail.com>
2015-10-14 19:15:11 -0700

    Edited string

--- Chillzone/Controllers/HomeController.cs
+++ Chillzone/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Chillzone.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Edited string.";
 
             return View();
         }
