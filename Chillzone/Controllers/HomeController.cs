0066d2451935c5381db86d682c2d01971d0e5b17
Banhawy <the.benhawy@gmail.com>
2015-10-14 19:39:40 -0700

    Batee5

--- Chillzone/Controllers/HomeController.cs
+++ Chillzone/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Chillzone.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Edited string. Again. GOOOOOOO";
+            ViewBag.Message = "Edited string. Again. Batee5";
 
             return View();
         }
