02780f6f51528cf8ec068b2d3785517f21cd44a4
Banhawy <the.benhawy@gmail.com>
2015-11-19 14:36:54 -0800

    included new homepage css in bundles

--- Chillzone/App_Start/BundleConfig.cs
+++ Chillzone/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Chillzone
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css", "~/Content/main.css",
+                      "~/Content/bootstrap.css", "~/Content/main.css","~/Content/new.css",
                       "~/Content/site.css"));
         }
     }
